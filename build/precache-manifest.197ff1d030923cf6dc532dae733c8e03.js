136f51bcd9626a1de8856a2fc2d87c519951f644
amorCui <43329030+amorCui@users.noreply.github.com>
2019-04-01 00:36:50 +0800

    build files

---
+++ build/precache-manifest.197ff1d030923cf6dc532dae733c8e03.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c309dedcc89300dc40f912146a30626",
+    "url": "./static/media/la.9c309ded.ogv"
+  },
+  {
+    "revision": "63f777697661931265e4",
+    "url": "./static/css/main.d959c4d2.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "06da44c2dffcfcb7c6f4",
+    "url": "./static/js/2.d80e1739.chunk.js"
+  },
+  {
+    "revision": "08b0bb7eaedf65b36ef2ab8a01f65e85",
+    "url": "./static/media/do.08b0bb7e.ogv"
+  },
+  {
+    "revision": "7502b5f898f8887fe484f6336e53ada8",
+    "url": "./static/media/re.7502b5f8.ogv"
+  },
+  {
+    "revision": "f7e6357434805f70610810efb71b5bae",
+    "url": "./static/media/mi.f7e63574.ogv"
+  },
+  {
+    "revision": "9db6dbc81401edae41ff5a8d9c7478b7",
+    "url": "./static/media/fa.9db6dbc8.ogv"
+  },
+  {
+    "revision": "77c9eda9389a074dc7152dd8387d753c",
+    "url": "./static/media/so.77c9eda9.ogv"
+  },
+  {
+    "revision": "63f777697661931265e4",
+    "url": "./static/js/main.038d587e.chunk.js"
+  },
+  {
+    "revision": "0f5ee7fd3018cd8adc42667102ec47ac",
+    "url": "./static/media/xi.0f5ee7fd.ogv"
+  },
+  {
+    "revision": "35cd5786e91d2b84d2becede667abf78",
+    "url": "./static/media/do2.35cd5786.ogv"
+  },
+  {
+    "revision": "3a6a51d2e048cba0b20d797ddb3e1c7c",
+    "url": "./static/media/do3.3a6a51d2.ogv"
+  },
+  {
+    "revision": "b5b7119a15831e64ce45729e79ac7a27",
+    "url": "./static/media/re3.b5b7119a.ogv"
+  },
+  {
+    "revision": "bf61633ff4f8ba5f087448c683038c82",
+    "url": "./static/media/mi3.bf61633f.ogv"
+  },
+  {
+    "revision": "c5c50bfa3e443b2cb98ecfd54848df3a",
+    "url": "./static/media/fa3.c5c50bfa.ogv"
+  },
+  {
+    "revision": "870a3654cc00f81efd13f8ead9491b0c",
+    "url": "./static/media/so3.870a3654.ogv"
+  },
+  {
+    "revision": "e14094dc82a401fd89aab9b5408a68cf",
+    "url": "./index.html"
+  }
+];
